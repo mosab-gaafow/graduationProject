f4b9448a23e2bb5a68ba6fb60d82361218e1e9c5
mosab-gaafow <devmuscab@gmail.com>
2025-04-20 15:13:48 +0300

    fixeewweq qfyhewfyqdqwdqw qw

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import job from './lib/cron.js';
 import tripRoutes from './routes/tripRoutes.js'
 // import bookingRoutes from './routes/bookingRoutes.js'
-// import vehicleRoutes from './routes/vehicleRoutes.js';
+import vehicleRoutes from './routes/vehicleRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config(); 
 
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes)
-// app.use('/api/vehicles', vehicleRoutes);
+app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/trips', tripRoutes);
 // app.use('/api/bookings', bookingRoutes);
 
